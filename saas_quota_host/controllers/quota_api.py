2b91ca0524f561ef76d7a0ffb6fdb72a6e094c46
EssghaierJacem <jacemsghaier3@gmail.com>
2025-06-23 19:19:28 +0100

    Db fix

--- saas_quota_host/controllers/quota_api.py
+++ saas_quota_host/controllers/quota_api.py
@@ -1,17 +1,21 @@
 from odoo import http
 from odoo.http import request
+import json
 
 class QuotaAPI(http.Controller):
     @http.route('/quota/api/v1/limits', type='http', auth='public', methods=['GET'], csrf=False)
     def get_limits(self, db_name=None, **kwargs):
-        client = request.env['saas.client'].sudo().search([('db_name', '=', db_name)], limit=1)
+        client = request.env['saas.client'].sudo().search([('database_name', '=', db_name)], limit=1)
         if not client:
             return http.Response(
-                '{"error": "not found"}',
+                json.dumps({"error": "not found"}),
                 status=404,
                 content_type='application/json'
             )
         return http.Response(
-            '{{"max_quotations": {}, "max_invoices": {}}}'.format(client.max_quotations, client.max_invoices),
+            json.dumps({
+                "max_quotations": client.max_quotations,
+                "max_invoices": client.max_invoices
+            }),
             content_type='application/json'
         ) 
