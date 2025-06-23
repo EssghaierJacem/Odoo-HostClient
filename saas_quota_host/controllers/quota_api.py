3cc05445134b3a1b5847e648b38dff2552ecb478
EssghaierJacem <jacemsghaier3@gmail.com>
2025-06-23 16:56:09 +0100

    QuotaControl

--- saas_quota_host/controllers/quota_api.py
+++ saas_quota_host/controllers/quota_api.py
@@ -4,10 +4,10 @@ from odoo.http import request
 class QuotaAPI(http.Controller):
     @http.route('/quota/api/v1/limits', type='json', auth='public', methods=['GET'], csrf=False)
     def get_limits(self, db_name=None, **kwargs):
-        quota = request.env['saas.client.quota'].sudo().search([('db_name', '=', db_name)], limit=1)
-        if not quota:
+        client = request.env['saas.client'].sudo().search([('db_name', '=', db_name)], limit=1)
+        if not client:
             return {'error': 'not found'}
         return {
-            'max_quotations': quota.max_quotations,
-            'max_invoices': quota.max_invoices,
+            'max_quotations': client.max_quotations,
+            'max_invoices': client.max_invoices,
         } 
--- saas_quota_host/models/saas_client_quota.py
+++ saas_quota_host/models/saas_client_quota.py
@@ -7,4 +7,10 @@ class SaasClientQuota(models.Model):
     client_name = fields.Char(required=True)
     db_name = fields.Char(required=True, index=True)
     max_quotations = fields.Integer(default=0)
-    max_invoices = fields.Integer(default=0) 
+    max_invoices = fields.Integer(default=0)
+
+class SaasClient(models.Model):
+    _inherit = 'saas.client'
+
+    max_quotations = fields.Integer(string="Max Quotations", default=0)
+    max_invoices = fields.Integer(string="Max Invoices", default=0) 
