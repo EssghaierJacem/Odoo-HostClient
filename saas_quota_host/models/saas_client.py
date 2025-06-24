ecb73081b14284ec5c86f64e8f1902589c25b394
EssghaierJacem <jacemsghaier3@gmail.com>
2025-06-24 16:51:15 +0100

    Menu Upgrade

--- saas_quota_host/models/saas_client.py
+++ saas_quota_host/models/saas_client.py
@@ -5,6 +5,7 @@ class SaasAbonnementType(models.Model):
     _description = 'Abonnement Type'
 
     name = fields.Char('Abonnement Name', required=True, index=True)
+    client_ids = fields.One2many('saas.client', 'abonnement_type_id', string='Clients')
 
 class SaasClient(models.Model):
     _inherit = 'saas.client'
@@ -16,7 +17,7 @@ class SaasClient(models.Model):
     invoice_price = fields.Float('Invoice Price', default=0.0)
     total_price = fields.Float('Total Price', compute='_compute_total_price', store=True)
 
-    @api.depends('quotation_price', 'invoice_price')
+    @api.depends('max_quotations', 'quotation_price', 'max_invoices', 'invoice_price')
     def _compute_total_price(self):
         for rec in self:
-            rec.total_price = rec.quotation_price + rec.invoice_price 
+            rec.total_price = (rec.max_quotations * rec.quotation_price) + (rec.max_invoices * rec.invoice_price) 
